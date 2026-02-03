0edc377f8b2e6c4d3c0310211ca6de166a46117e
Mayank Soni <202451104@iiitvadodara.ac.in>
2026-02-04 01:44:35 +0530

    adding

--- backend/routes/leaderBoardRoute.js
+++ backend/routes/leaderBoardRoute.js
@@ -1,7 +1,8 @@
 import express from "express"
 import { getUser } from "../controllers/leaderBoard";
+import { protect } from "../middleware/authMiddleware";
 
 const router = express.Router();
-router.get('/leaderboard',getUser);
+router.get('/leaderboard',protect,getUser);
 
 export default router;
